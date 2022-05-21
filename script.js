8c4846f009057524db6591018d7303ef0ca08a18
VykhaVijay <83864456+VykhaVijay@users.noreply.github.com>
2022-05-22 00:37:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('#cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>
+{
+    document.querySelector('.firstdiv').classList.toggle('firstdivision');
+})
+
